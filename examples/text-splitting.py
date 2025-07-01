dd64c67d817e6343fb512fa776c02cb46c259491
Sérgio Freitas <psergiocf@gmail.com>
2025-07-01 12:24:47 +0100

    feat: add example to test text splitting (chunks)

---
+++ examples/text-splitting.py
@@ -0,0 +1,34 @@
+# This snippet was being used on Colab to format the output of the text splitting examples.
+
+# #@title Setup better response formatting (adds line wrap)
+# from IPython.display import HTML, display
+
+# def set_css():
+#   display(HTML('''
+#   <style>
+#     pre {
+#         white-space: pre-wrap;
+#     }
+#   </style>
+#   '''))
+# get_ipython().events.register('pre_run_cell', set_css)
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain_community.document_loaders import PyPDFLoader
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+loader = PyPDFLoader("docs/bitcoin.pdf")
+docs = loader.load()
+
+# Different chunk sizes (as well as different chunk overlaps) generate different documents
+for chunk_size in [250, 500, 750, 1000]: # 500 - 1000
+# for chunk_size in [10, 20, 50]:
+  chunk_overlap = chunk_size * 0.2 # 15% - 30%
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+  # docs = [Document(page_content=x) for x in text_splitter.split_text(text)]
+  chunks = text_splitter.split_documents(docs)
+  print("-" * 80)
+  print(len(chunks))
+  print(f"Chunk size {chunk_size} | Chunk overlap {chunk_overlap}")
+  print(f"Documents: {chunks}")
