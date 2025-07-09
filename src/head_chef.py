c0721d014dc86b0003f5b24e8ce30b050da50adb
Sérgio Freitas <psergiocf@gmail.com>
2025-07-09 20:46:42 +0100

    the agent now uses history and filters the relevant chunks based on the query

--- src/head_chef.py
+++ src/head_chef.py
@@ -1,14 +1,17 @@
 import gradio as gr
+from history_manager import update_history
 from recipes_loader import load_recipes
 from recipes_chunker import chunk_recipes
-from recipes_db import create_vector_db
+from recipes_db import create_vector_db, add_documents_to_vector_db, get_relevant_chunks
 from langchain_openai import ChatOpenAI         # LLM
 
+vector_db = create_vector_db()
 
-def predict(message, history):
-    ################################################################
-    ########## Phase 1: Load information and prepare RAG ###########
-    ################################################################
+
+################################################################
+########## Phase 1: Load information and prepare RAG ###########
+################################################################
+def load_information():
     # Load recipes from JSON file
     recipes = load_recipes()
 
@@ -16,26 +19,25 @@ def predict(message, history):
     chunks = chunk_recipes(recipes)
 
     # Embed the chunks and store them in a vector database
-    vector_db = create_vector_db(chunks)
+    add_documents_to_vector_db(vector_db, chunks)
 
 
-
-    ################################################################
-    ############# Phase 2: RAG Retrieval Augmentation ##############
-    ################################################################
-
+################################################################
+############# Phase 2: RAG Retrieval Augmentation ##############
+################################################################
+def predict(message, history):
     user_query = message
 
-    # Step 2.1 - Retrieve dos chunks mais relevantes
-    # Custos
-    relevant_chunks = vector_db.similarity_search(user_query, 3)
-    print(relevant_chunks)
+    relevant_chunks = get_relevant_chunks(vector_db, user_query, 3)
+    full_history = update_history(history)
 
-    # Step 2.2 Create final prompt to send to LLM
+
+
     prompt = f"""
         Instructions:
-        Answer the provided user query with the given context
-        Only answer questions that the answer is provided in the given context
+        Answer the provided user query
+        Use the provided context, chat history, or both to deduce the answer.
+        The chat history is ordered from the first to the latest interaction, the user is a message from the user, the assistant is a message from the LLM.
         If you don't know the answer, say 'I don't know'
 
         User query:
@@ -43,6 +45,9 @@ def predict(message, history):
 
         Context:
         {relevant_chunks}
+
+        Chat History:
+        {full_history}
     """
 
     # Step 2.3 Send the prompt to the LLM and get the final response
@@ -69,6 +74,6 @@ def predict(message, history):
     # return "Hi Sérgio, I'm the Head Chef AI!"
 
 # Load the information and prepare the RAG
-# vector_db = load_information()
+# load_information()
 
 gr.ChatInterface(predict, type="messages").launch(debug=True)
--- src/recipes_db.py
+++ src/recipes_db.py
@@ -4,28 +4,40 @@ from langchain_chroma import Chroma
 
 # Generate embedding model
 # https://python.langchain.com/docs/integrations/text_embedding/openai/
-# Custos dependentes de quantos tokens temos nos nossos chunks
-# NÃO SE ESQUEÇAM QUE ESTA FASE EXISTE, E TEM CUSTOS, SIMPLESMENTE ESTÁ INTEGRADA NA PRÓXIMA
 def generate_embedding_model():
     return OpenAIEmbeddings(
         openai_api_key=os.getenv('OPENAI_API_KEY'),
         model="text-embedding-3-small"
     )
 
-def generate_vector_db(embedding_model):
-    return Chroma(
+# Create a vector database using Chroma
+def create_vector_db():
+    embedding_model = generate_embedding_model()
+    vector_db = Chroma(
         collection_name="head-chef-recipes",
         embedding_function=embedding_model,
         persist_directory="vector_db"
     )
 
-def create_vector_db(chunks):
-    embedding_model = generate_embedding_model()
-    vector_db = generate_vector_db(embedding_model)
+    return vector_db
 
+# Add documents to the vector database
+def add_documents_to_vector_db(vector_db, chunks):
     # Don't forget that this is where the embedding happens, the vectors are stored and there are associated costs
     vector_db.add_documents(chunks)
 
     print(f"Vector DB: {vector_db}")
 
     return vector_db
+
+# Retrieve relevant chunks based on user query
+def get_relevant_chunks(vector_db, user_query, top_k=3):
+    distance_threshold = 1.5
+    relevant_chunks = ""
+    chunks = vector_db.similarity_search_with_score(user_query, top_k)
+
+    for chunk in chunks:
+        if chunk[1] < distance_threshold:
+            relevant_chunks = relevant_chunks + "\n" + chunk[0].page_content
+
+    return relevant_chunks
