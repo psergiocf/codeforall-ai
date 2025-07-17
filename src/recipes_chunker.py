939757d947e1c06dc2705a6d8995d17de9120552
Sérgio Freitas <psergiocf@gmail.com>
2025-07-17 23:07:47 +0100

    add index to document's metadata

--- src/recipes_chunker.py
+++ src/recipes_chunker.py
@@ -6,8 +6,10 @@ def fix_typos(page_content):
     return page_content.replace("rattings", "ratings")
 
 # Returns an object with the recipe"s filtering data
-def prepare_recipe_metadata(recipe):
+def prepare_recipe_metadata(index, recipe):
     filter = {}
+
+    filter["index"] = index
 
     if "id" in recipe:
         filter["id"] = recipe["id"]
@@ -45,11 +47,11 @@ def prepare_recipe_metadata(recipe):
 def update_recipes_data(recipes):
     new_recipes = []
 
-    for recipe in recipes:
+    for index, recipe in enumerate(recipes):
         initial_page_content = recipe.page_content
 
         updated_page_content = fix_typos(initial_page_content)
-        metadata = prepare_recipe_metadata(json.loads(updated_page_content))
+        metadata = prepare_recipe_metadata(index, json.loads(updated_page_content))
 
         new_recipes.append(
             Document(
